d50a5e11a733730a91fce55cef4ad0fe3fb75d63
DanielUmetor <dnumetor@gmail.com>
2024-05-24 13:51:06 +0200

    functionExamples

--- script/code.js
+++ script/code.js
@@ -1,10 +1,24 @@
-let arr1 = [1, 4, 5]
+// let arr1 = [1, 4, 5]
 // let arr2 = [...arr1]
-let arr2 = Array.from(arr1) // Array.from is copying all the elements from array1 to array2
-arr2[0] = 10
-console.log('arr1: ', arr1);
-console.log('arr2: ', arr2);
+// let arr2 = Array.from(arr1) // Array.from is copying all the elements from array1 to array2
+// arr2[0] = 10
+// console.log('arr1: ', arr1);
+// console.log('arr2: ', arr2);
 // shallow copy 
 // deep copy creates a new array
 // console.log(...arr1); // ... is called a spread operator or making a copy of one array to another array.
-console.log("Our lecturer's name is Joel");
+// console.log('Our lecturer\'s name is Joel'); //   \- escape character
+
+(function addition (numb1, numb2) {
+    console.log(numb1 + numb2);
+})(4, 2)
+// Recursive function
+function loop(cnt, limit) {
+    if (cnt > limit) {
+        return
+    } else {
+        console.log(cnt);
+        loop(cnt + 1, limit)
+    }
+}
+loop(0)
